1f7a019cdc5147369d1678020cea0d7910dc7ca0
Tuan Tran Van <tuantran1708@gmail.com>
2016-03-27 04:24:50 +0700

    receive data, not tested

--- Ximala_UWP/View/StreetLightStatusPage.xaml.cs
+++ Ximala_UWP/View/StreetLightStatusPage.xaml.cs
@@ -21,6 +21,7 @@ namespace Ximala_UWP.View
         public LightStatusViewModel Vm => (LightStatusViewModel)DataContext;
         
         private StreamSocket _socket;
+        private StreamSocketListener _socketListener;
 
         private RfcommDeviceService _service;
 
@@ -41,8 +42,7 @@ namespace Ximala_UWP.View
 
                 var device = devices.Single(x => x.Name == "HC-05");
 
-                _service = await RfcommDeviceService.FromIdAsync(
-                    device.Id);
+                _service = await RfcommDeviceService.FromIdAsync(device.Id);
 
                 _socket = new StreamSocket();
 
@@ -50,11 +50,61 @@ namespace Ximala_UWP.View
                     _socket.ConnectAsync(_service.ConnectionHostName, _service.ConnectionServiceName,
                         SocketProtectionLevel.BluetoothEncryptionAllowNullAuthentication);
                 Vm.ConnectionStatus = _service.ConnectionHostName + " | " + _service.ConnectionServiceName;
+
+                Vm.ConnectionStatus = "Start listener";
+
+                _socketListener = new StreamSocketListener();
+
+                await _socketListener.BindServiceNameAsync(_service.ServiceId.AsString());
+                _socketListener.ConnectionReceived += _socketListener_ConnectionReceived;
+
+                Vm.ConnectionStatus = "Listenning";
             }
             catch (Exception ex)
             {
                 //tbError.Text = ex.Message;
                 Vm.ConnectionStatus = ex.Message;
+            }
+        }
+
+        private async void _socketListener_ConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            DataReader reader = new DataReader(args.Socket.InputStream);
+            try
+            {
+                while (true)
+                {
+                    // Read first 4 bytes (length of the subsequent string).
+                    uint sizeFieldCount = await reader.LoadAsync(sizeof(uint));
+                    if (sizeFieldCount != sizeof(uint))
+                    {
+                        // The underlying socket was closed before we were able to read the whole data.
+                        return;
+                    }
+
+                    // Read the string.
+                    uint stringLength = reader.ReadUInt32();
+                    uint actualStringLength = await reader.LoadAsync(stringLength);
+                    if (stringLength != actualStringLength)
+                    {
+                        // The underlying socket was closed before we were able to read the whole data.
+                        return;
+                    }
+
+                    // Display the string on the screen. The event is invoked on a non-UI thread, so we need to marshal
+                    // the text back to the UI thread.
+                    Vm.ConnectionStatus = String.Format("Received data: \"{0}\"", reader.ReadString(actualStringLength));
+                }
+            }
+            catch (Exception exception)
+            {
+                // If this is an unknown status it means that the error is fatal and retry will likely fail.
+                if (SocketError.GetStatus(exception.HResult) == SocketErrorStatus.Unknown)
+                {
+                    throw;
+                }
+
+                Vm.ConnectionStatus = "Listening Failed: " + exception.Message;
             }
         }
 
