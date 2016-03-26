05eec5b28a95ab8269dac4aca1deae5b80c39b5b
Tuan Tran Van <tuantran1708@gmail.com>
2016-03-26 21:09:02 +0700

    add view model for light status page

--- Ximala_UWP/View/StreetLightStatusPage.xaml.cs
+++ Ximala_UWP/View/StreetLightStatusPage.xaml.cs
@@ -1,30 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+﻿using Ximala_UWP.ViewModel;
 
 namespace Ximala_UWP.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class StreetLightStatusPage : Page
+    public sealed partial class StreetLightStatusPage
     {
+        /// <summary>
+        /// Gets the view's ViewModel.
+        /// </summary>
+        public LightStatusViewModel Vm => (LightStatusViewModel)DataContext;
+
         public StreetLightStatusPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
     }
 }
